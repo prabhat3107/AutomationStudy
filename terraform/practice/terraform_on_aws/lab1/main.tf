c1d189760345d9e830fad232d73c95d9eb3a1124
Prabhat R Pradhan <prabhat3107@gmail.com>
2024-12-24 19:30:20 -0500

    New EC2 instance creation using first terraform code

--- terraform/practice/terraform_on_aws/lab1/main.tf
+++ terraform/practice/terraform_on_aws/lab1/main.tf
@@ -13,8 +13,44 @@ provider "aws" {
 
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type = number
+  default     = 8090
+}
 
 resource "aws_instance" "pp_linux_instance" {
-  ami = "ami-0e2c8caa4b6378d8c"
-  instance_type = "t2.micro"
+  ami                    = "ami-0e2c8caa4b6378d8c"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.terraform_instances.id}"]
+  key_name = "pp_terraform_ssh_key"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello World!" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "terraform_instances" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = "${aws_instance.pp_linux_instance.public_ip}"
 }
